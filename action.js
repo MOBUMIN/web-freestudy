948bfb83f5b080485eb3529b65b7bf448fe5f610
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-10-15 21:51:37 +0900

    사이드바, 컨텐트 박스
    js 하기 싫어서 이러고 있는중

--- action.js
+++
@@ -1,8 +0,0 @@
-function on_menu(){
-    $('.menu-text').css('display','none');
-    $('.nav').css('display','block');
-}
-function off_menu(){
-    $('.menu-text').css('display','inline');
-    $('.nav').css('display','none');
-}
