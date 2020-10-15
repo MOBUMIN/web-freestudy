ec7345abf6803a11979f52ec59b96e041f4814b5
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-10-15 18:34:40 +0900

    menu on/off
    메뉴에 마우스 가져다대면 first second로 변경

---
+++ action.js
@@ -0,0 +1,8 @@
+function on_menu(){
+    $('.menu-text').css('display','none');
+    $('.nav').css('display','block');
+}
+function off_menu(){
+    $('.menu-text').css('display','inline');
+    $('.nav').css('display','none');
+}
