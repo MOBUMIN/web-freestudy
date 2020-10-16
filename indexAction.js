3651be68435305c45f3d87fa8f2c9c33385e9633
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-10-16 14:43:33 +0900

    html 분리
    *제이쿼리로 헤더, 푸터, 내용 다 분리

---
+++ indexAction.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html"); 
+    $("#first").load("html/first.html");
+    $("#second").load("html/second.html");
+    $("#third").load("html/third.html");
+  });
